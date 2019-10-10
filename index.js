e616ebe18bf9535312a067cc81fb7eba4b59a1a1
pablomag <pablomag@gmail.com>
2019-10-10 21:07:17 +0300

    feat(init): Track generation and movement (speed, width, margin)

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 const car = document.getElementById('car');
 const viewport = document.getElementById('viewport');
 
+const viewportHeight = parseInt(viewport.offsetHeight);
+const trackHeight = 19;
+const trackChunks = 40; /* Track chunks to pre-render */
+
 const unit = 'px';
 const gameTick = 5;
-const trackSpeed = .1;
+const trackSpeed = 1; /* 1 to 10 */
 
 const steeringSpeed = 3;
 const gasSpeed = 1;
@@ -14,44 +18,16 @@ let pedal = 'none';
 let steering = false;
 let gasPressed = false;
 
+const startingTrackWidth = 400;
+const widenSpeed = 3;
+let trackWidth = 150;
+let trackMargin = 140;
+
 const keyCodes = {
 	37: 'left',
 	38: 'gas',
 	39: 'right',
 	40: 'brake'
-};
-
-let initGame = false;
-
-const init = () => {
-
-	if (!initGame) {
-
-		initGame = true;
-
-		const overlay = document.getElementById('overlay');
-		const tracks = document.querySelectorAll('.track');
-		const trackId = tracks.length;
-
-		const track = document.createElement('div');
-		const leftRail = document.createElement('div');
-		const road = document.createElement('div');
-		const rightRail = document.createElement('div');
-	
-		track.style.top = '-1em';
-		track.style.left = '0';
-		track.classList.add('track', `track-${trackId}`);
-	
-		overlay.appendChild(track);
-	
-		leftRail.classList.add('left-rail');
-		road.classList.add('road');
-		rightRail.classList.add('right-rail');
-	
-		track.appendChild(leftRail);
-		track.appendChild(road);
-		track.appendChild(rightRail);
-	}
 };
 
 const drive = (event) => {
@@ -191,34 +167,169 @@ const crashDetection = () => {
 	});
 };
 
-const travel = (track) => {
+const getTopValue = () => {
 
-	let top = track.style.top;
-	const currentPosition = parseFloat(top.replace(unit, ''));
+	const numberOfTracks = getNumberOfTracks();
 
-	top = !top ? trackSpeed : currentPosition + trackSpeed;
+	return viewportHeight - (trackHeight * numberOfTracks);
+};
+
+const getNumberOfTracks = () => {
+
+	const tracks = document.querySelectorAll('.track');
+
+	return tracks.length;
+};
+
+const createTrack = () => {
+
+	const track = document.createElement('div');
+	const leftRail = document.createElement('div');
+	const road = document.createElement('div');
+	const rightRail = document.createElement('div');
+
+	const top = getTopValue();
+	const numberOfTracks = getNumberOfTracks();
+
+	track.style.top = `${top}${unit}`;
+	track.classList.add('track', `track-${numberOfTracks}`);
+
+	leftRail.classList.add('left-rail');
+	road.classList.add('road');
+	rightRail.classList.add('right-rail');
+
+	road.style.width = `${startingTrackWidth}${unit}`;
+
+	track.appendChild(leftRail);
+	track.appendChild(road);
+	track.appendChild(rightRail);
+
+	const trackContainer = document.getElementById('tracks');
+
+	trackContainer.appendChild(track);
+};
+
+let currentTrackWidth = 0;
+let currentTrackMargin = 0;
+
+const recycleTrack = (track) => {
+
+	let top = getTopValue();
 
 	track.style.top = `${top}${unit}`;
 
-	if (!overlaps(track, viewport))
-		track.remove();
+	const road = track.querySelector('.road');
+
+	if (currentTrackWidth === 0) {
+
+		currentTrackWidth = road.style.width;
+		currentTrackWidth = parseInt(currentTrackWidth.replace(unit, ''));
+	}
+
+	let newRoadWidth = currentTrackWidth;
+
+	if (trackWidth > currentTrackWidth)
+		newRoadWidth = (currentTrackWidth + widenSpeed) > trackWidth ? trackWidth : (currentTrackWidth + widenSpeed);
+
+	if (trackWidth < currentTrackWidth)
+		newRoadWidth = (currentTrackWidth - widenSpeed) < trackWidth ? trackWidth : (currentTrackWidth - widenSpeed);
+
+	currentTrackWidth = newRoadWidth;
+
+	road.style.width = `${newRoadWidth}${unit}`;
+
+	let newTrackMargin = currentTrackMargin;
+
+	if (trackMargin > currentTrackMargin)
+		newTrackMargin = (currentTrackMargin + widenSpeed) > trackMargin ? trackMargin : (currentTrackMargin + widenSpeed);
+
+	if (trackMargin < currentTrackMargin)
+		newTrackMargin = (currentTrackMargin - widenSpeed) < trackMargin ? trackMargin : (currentTrackMargin - widenSpeed);
+
+	currentTrackMargin = newTrackMargin;
+
+	track.style.left = `${currentTrackMargin}${unit}`;
 };
 
-setInterval(() => {
-	init();
-	carMovement();
-	trackTravel();
-	crashDetection();
-}, gameTick);
+const travel = (track) => {
+
+	let top = track.style.top;
+	const currentPosition = parseInt(top.replace(unit, ''));
+
+	top = !top ? 0 : currentPosition + trackSpeed;
+
+	track.style.top = `${top}${unit}`;
+
+	if (top > viewportHeight) recycleTrack(track);
+
+	const classList = track.classList.value;
+	const classes = classList.split(' ');
+
+	const t1 = document.getElementById('track-0-display');
+	const t2 = document.getElementById('track-1-display');
+	const t3 = document.getElementById('track-2-display');
+	const t4 = document.getElementById('track-3-display');
+	const t5 = document.getElementById('track-4-display');
+
+	if (classes.includes('track-0'))
+		t1.value = `top: ${top}, ${viewportHeight} = ${top > viewportHeight}`;
+	if (classes.includes('track-1'))
+		t2.value = `top: ${top}, ${viewportHeight} = ${top > viewportHeight}`;
+	if (classes.includes('track-2'))
+		t3.value = `top: ${top}, ${viewportHeight} = ${top > viewportHeight}`;
+	if (classes.includes('track-3'))
+		t4.value = `top: ${top}, ${viewportHeight} = ${top > viewportHeight}`;
+	if (classes.includes('track-4'))
+		t5.value = `top: ${top}, ${viewportHeight} = ${top > viewportHeight}`;
+};
+
+let ready = true;
 
 const trackTravel = () => {
 
 	const tracks = document.querySelectorAll('.track');
 
-	tracks.forEach((track) => {
-		travel(track);
-	});
+	let tracksProcessed = 0;
+
+	if (ready) {
+
+		ready = false;
+
+		tracks.forEach((track, index, array) => {
+
+			travel(track);
+
+			tracksProcessed++;
+
+			if (tracksProcessed === array.length) ready = true;
+		});
+	}
 };
 
-window.addEventListener('keydown', drive);
-window.addEventListener('keyup', stop);
+const start = () => {
+
+	window.addEventListener('keydown', drive);
+	window.addEventListener('keyup', stop);
+
+	setInterval(() => {
+		trackTravel();
+		carMovement();
+		crashDetection();
+	}, gameTick);
+};
+
+const init = () => {
+
+	let tracksCreated = 0;
+
+	while (tracksCreated <= trackChunks) {
+
+		createTrack();
+
+		tracksCreated++;
+
+		if (tracksCreated === trackChunks) start();
+	}
+};
+
+init();
